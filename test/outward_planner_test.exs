129b97c3e36b79d55398395d960115172f719b7c
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-18 21:23:26 +0200

    more tests

--- test/outward_planner_test.exs
+++ test/outward_planner_test.exs
@@ -6,10 +6,17 @@ defmodule OutwardPlannerTest do
     assert OutwardPlanner.main(args) == :ok
   end
 
-  test "build category members query" do
-    with query <- %OutwardPlanner.Query{cmtitle: :daggers} do
-      assert OutwardPlanner.Query.build(query) ==
+  test "build category members list query" do
+    with query <- %OutwardPlanner.Query.Category{cmtitle: :daggers} do
+      assert OutwardPlanner.Query.Category.build(query) ==
                "https://outward.wiki.gg/api.php?action=query&list=categorymembers&cmtitle=Category:Daggers&cmlimit=max&format=json"
+    end
+  end
+
+  test "build category member query" do
+    with query <- %OutwardPlanner.Query.Page{pageids: 2259} do
+      assert OutwardPlanner.Query.Page.build(query) ==
+               "https://outward.wiki.gg/api.php?action=query&pageids=2259&format=json"
     end
   end
 end
