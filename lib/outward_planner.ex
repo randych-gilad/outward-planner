707e6a08187583d93798e5cb412ff141b34a4514
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-18 21:23:39 +0200

    page query

--- lib/outward_planner.ex
+++ lib/outward_planner.ex
@@ -1,4 +1,6 @@
 defmodule OutwardPlanner do
+  def base_url, do: "https://outward.wiki.gg/api.php?"
+
   def main(_args) do
     :ok
   end
--- lib/outward_planner/query.ex
+++ lib/outward_planner/query.ex
@@ -1,7 +1,8 @@
-defmodule OutwardPlanner.Query do
+defmodule OutwardPlanner.Query.Category do
   @moduledoc """
-  Build Wiki.gg REST API query to be used in other modules.
+  Build Wiki.gg REST API query for category to be used in other modules.
   """
+  @enforce_keys :cmtitle
   @type t :: %__MODULE__{
           base_url: String.t(),
           action: atom(),
@@ -10,7 +11,7 @@ defmodule OutwardPlanner.Query do
           cmlimit: atom(),
           format: atom()
         }
-  defstruct base_url: "https://outward.wiki.gg/api.php?",
+  defstruct base_url: OutwardPlanner.base_url(),
             action: :query,
             list: :categorymembers,
             cmtitle: :empty,
@@ -32,5 +33,56 @@ defmodule OutwardPlanner.Query do
            to_string(params.format)
        ]
        |> Enum.join("&"))
+  end
+end
+
+defmodule OutwardPlanner.Query.Page do
+  @moduledoc """
+  Build Wiki.gg REST API query for page result by id.
+  """
+  @enforce_keys :pageids
+  @type t :: %__MODULE__{
+          base_url: String.t(),
+          action: atom(),
+          pageids: non_neg_integer() | [non_neg_integer()],
+          format: atom()
+        }
+  defstruct base_url: OutwardPlanner.base_url(),
+            action: :query,
+            pageids: 0,
+            prop: :revisions,
+            rvprop: :content,
+            rvslots: :main,
+            format: :json
+
+  # https://outward.wiki.gg/api.php?action=query&pageids=2259&format=json
+  # https://outward.wiki.gg/api.php?action=query&pageids=2259&prop=revisions&rvprop=content&rvslots=main&format=json
+  def build(%__MODULE__{} = params) do
+    params.base_url <>
+      ([
+         "action=" <>
+           to_string(params.action),
+         "pageids=" <>
+           parse_ids(params.pageids),
+         "prop=" <>
+           to_string(params.prop),
+         "rvprop=" <>
+           to_string(params.rvprop),
+         "rvslots=" <>
+           to_string(params.rvslots),
+         "format=" <>
+           to_string(params.format)
+       ]
+       |> Enum.join("&"))
+  end
+
+  defp parse_ids(page_ids) when is_list(page_ids) do
+    page_ids
+    |> Enum.join("|")
+  end
+
+  defp parse_ids(page_ids) when is_integer(page_ids) do
+    page_ids
+    |> to_string()
   end
 end
