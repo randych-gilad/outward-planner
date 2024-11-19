858754661202fad9a33fef343eca22bae1a5d872
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-19 12:24:49 +0200

    correct test to account for float instead of int

--- test/outward_planner_test.exs
+++ test/outward_planner_test.exs
@@ -2,10 +2,12 @@ defmodule OutwardPlannerTest do
   use ExUnit.Case
   doctest OutwardPlanner
 
+  @tag :local
   test "default", args do
     assert OutwardPlanner.main(args) == :ok
   end
 
+  @tag :local
   test "build category members list query" do
     with query <- %OutwardPlanner.Query.Category{cmtitle: :daggers} do
       assert OutwardPlanner.Query.Category.build(query) ==
@@ -13,6 +15,7 @@ defmodule OutwardPlannerTest do
     end
   end
 
+  @tag :local
   test "build category member query" do
     with query <- %OutwardPlanner.Query.Page{pageids: 2259} do
       assert OutwardPlanner.Query.Page.build(query) ==
@@ -20,35 +23,36 @@ defmodule OutwardPlannerTest do
     end
   end
 
+  @tag :api
   test "get Zhorn's dagger struct" do
     with query <- OutwardPlanner.ApiRequest.request_page(:daggers),
          zhorn_dagger <-
            Enum.find(query, fn map -> map.name == "Zhorn's Glowstone Dagger" end) do
       assert zhorn_dagger == %OutwardPlanner.Stats.Weapon{
-               name: "Zhorn's Glowstone Dagger",
-               class: "Dagger",
-               type: "Off-Handed",
-               set: "Zhorn's Set",
-               protection: 0,
+               attackspeed: 1.0,
                barrier: 0,
-               impact_resistance: 0,
-               mana_cost_reduction: 0,
-               attackspeed: 1,
-               effects: "Glowing (Light Source)",
-               impact: 38,
-               physical: 20,
+               class: "Dagger",
                decay: 0,
-               ethereal: 0,
-               fire: 0,
-               frost: 0,
-               lightning: 9,
-               stamcost: 0,
-               physical_bonus: 0,
                decay_bonus: 0,
+               effects: "Glowing (Light Source)",
+               ethereal: 0,
                ethereal_bonus: 0,
+               fire: 0,
                fire_bonus: 0,
+               frost: 0,
                frost_bonus: 0,
-               lightning_bonus: 0
+               impact: 38.0,
+               impact_resistance: 0,
+               lightning: 10.0,
+               lightning_bonus: 0,
+               mana_cost_reduction: 0,
+               name: "Zhorn's Glowstone Dagger",
+               physical: 20.0,
+               physical_bonus: 0,
+               protection: 0,
+               set: "Zhorn's Set",
+               stamcost: 0,
+               type: "Off-Handed"
              }
     end
   end
