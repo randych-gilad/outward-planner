0b86cc4d9c6242e0ecd5873d3a9131fd15ab6475
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-19 11:28:54 +0200

    struct test

--- test/outward_planner_test.exs
+++ test/outward_planner_test.exs
@@ -19,4 +19,37 @@ defmodule OutwardPlannerTest do
                "https://outward.wiki.gg/api.php?action=query&pageids=2259&prop=revisions&rvprop=content&rvslots=main&format=json"
     end
   end
+
+  test "get Zhorn's dagger struct" do
+    with query <- OutwardPlanner.ApiRequest.request_page(:daggers),
+         zhorn_dagger <-
+           Enum.find(query, fn map -> map.name == "Zhorn's Glowstone Dagger" end) do
+      assert zhorn_dagger == %OutwardPlanner.Stats.Weapon{
+               name: "Zhorn's Glowstone Dagger",
+               class: "Dagger",
+               type: "Off-Handed",
+               set: "Zhorn's Set",
+               protection: 0,
+               barrier: 0,
+               impact_resistance: 0,
+               mana_cost_reduction: 0,
+               attackspeed: 1,
+               effects: "Glowing (Light Source)",
+               impact: 38,
+               physical: 20,
+               decay: 0,
+               ethereal: 0,
+               fire: 0,
+               frost: 0,
+               lightning: 9,
+               stamcost: 0,
+               physical_bonus: 0,
+               decay_bonus: 0,
+               ethereal_bonus: 0,
+               fire_bonus: 0,
+               frost_bonus: 0,
+               lightning_bonus: 0
+             }
+    end
+  end
 end
