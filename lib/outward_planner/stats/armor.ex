c8b0c6eb8759601fa2f1caf8fa14ce4ebae8f50a
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-20 03:24:22 +0200

    armor struct

---
+++ lib/outward_planner/stats/armor.ex
@@ -0,0 +1,34 @@
+defmodule OutwardPlanner.Stats.Armor do
+  @moduledoc """
+  Armor struct.
+
+  Elements without bonus refer to resistance.
+
+  Elements with bonus refer to % damage increase.
+  """
+  defstruct name: "",
+            class: "",
+            type: "",
+            set: "",
+            protection: 0,
+            barrier: 0,
+            mana_cost_reduction: 0,
+            stamina_cost_reduction: 0,
+            movespeed: 0,
+            decay: 0,
+            decay_bonus: 0,
+            effects: "",
+            ethereal: 0,
+            ethereal_bonus: 0,
+            fire: 0,
+            fire_bonus: 0,
+            frost: 0,
+            frost_bonus: 0,
+            impact: 0,
+            lightning: 0,
+            lightning_bonus: 0,
+            physical: 0,
+            physical_bonus: 0,
+            coldresist: 0,
+            heatresist: 0
+end
--- lib/outward_planner/stats/weapon.ex
+++ lib/outward_planner/stats/weapon.ex
@@ -1,4 +1,11 @@
 defmodule OutwardPlanner.Stats.Weapon do
+  @moduledoc """
+  Weapon struct.
+
+  Elements without bonus refer to flat damage.
+
+  Elements with bonus refer to % damage increase.
+  """
   defstruct name: "",
             class: "",
             type: "",
@@ -7,20 +14,20 @@ defmodule OutwardPlanner.Stats.Weapon do
             barrier: 0,
             impact_resistance: 0,
             mana_cost_reduction: 0,
+            stamcost: 0,
             attackspeed: 0,
             effects: "",
             impact: 0,
             physical: 0,
-            decay: 0,
-            ethereal: 0,
-            fire: 0,
-            frost: 0,
-            lightning: 0,
-            stamcost: 0,
             physical_bonus: 0,
+            decay: 0,
             decay_bonus: 0,
             ethereal_bonus: 0,
+            ethereal: 0,
+            fire: 0,
             fire_bonus: 0,
+            frost: 0,
             frost_bonus: 0,
+            lightning: 0,
             lightning_bonus: 0
 end
