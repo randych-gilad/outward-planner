2c66f0ffec090b818b80d21715c7c78a1ead04f9
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-21 04:17:22 +0200

    filter out category named page

--- lib/outward_planner/query/parser.ex
+++ lib/outward_planner/query/parser.ex
@@ -5,11 +5,10 @@ defmodule OutwardPlanner.Query.Parser do
   alias OutwardPlanner.Stats
   alias __MODULE__.WikiAPIError
 
-  # This is not useful because it is usable in ApiRequest.request_category which is unrelated side effect
-  @mainhand [:axes, :bows, :maces, :polearms, :spears, :swords, :gauntlets]
-  @offhand [:chakrams, :daggers, :pistols, :lanterns, :lexicons, :shields]
+  @mainhand ~w(Axes Bows Maces Polearms Spears Swords Gauntlets)
+  @offhand ~w(Chakrams Daggers Pistols Lanterns Lexicons Shields)
   @weapons @mainhand ++ @offhand
-  @armor [:helmets, :body_armor, :boots, :backpacks]
+  @armor ~w(Helmets Body Armor Boots Backpacks) ++ ["Body Armor"]
 
   def decode_page_content!(body) do
     body
@@ -50,7 +49,8 @@ defmodule OutwardPlanner.Query.Parser do
       |> Enum.into(%{})
       |> Map.put(:name, title)
     end)
-    |> Enum.map(&Map.filter(&1, fn {_k, v} -> v != "" end))
+    |> Enum.reject(&exclude_category_page/1)
+    |> Enum.map(&exclude_empty_values/1)
     |> Enum.map(&struct!(%Stats.Weapon{}, &1))
   end
 
@@ -73,20 +73,20 @@ defmodule OutwardPlanner.Query.Parser do
     ])
   end
 
-  def format_stats(stat) when is_binary(stat) do
+  defp format_stats(stat) when is_binary(stat) do
     stat
     |> String.replace(["[[", "]]"], "")
     |> String.split("=")
     |> Enum.map(&String.trim/1)
   end
 
-  def format_struct_key(key) when is_binary(key) do
+  defp format_struct_key(key) when is_binary(key) do
     key
     |> Macro.underscore()
     |> String.to_atom()
   end
 
-  def format_number(number) when is_binary(number) do
+  defp format_number(number) when is_binary(number) do
     case Float.parse(number) do
       :error ->
         number
@@ -94,5 +94,14 @@ defmodule OutwardPlanner.Query.Parser do
       {parsed_number, _} ->
         Float.round(parsed_number)
     end
+  end
+
+  defp exclude_category_page(page) do
+    page.name in @weapons
+  end
+
+  defp exclude_empty_values(pages) do
+    pages
+    |> Map.filter(fn {_k, v} -> v != "" end)
   end
 end
