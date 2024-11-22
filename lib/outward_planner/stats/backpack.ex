6c714c1a71db6763f815c5f9e8e4008dc7764a17
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-22 14:48:39 +0200

    backpack schema

--- lib/outward_planner/stats/backpack.ex
+++ lib/outward_planner/stats/backpack.ex
@@ -4,18 +4,22 @@ defmodule OutwardPlanner.Stats.Backpack do
 
   Invent_prot refers to inventory protection.
   """
-  defstruct name: "",
-            set: "",
-            class: "",
-            capacity: 0,
-            protection: 0,
-            invent_prot: 0,
-            status_resist: 0,
-            movespeed: 0,
-            stamina_cost_reduction: 0,
-            effects: "",
-            preservation: 0,
-            physical_bonus: 0,
-            lightning_bonus: 0,
-            corruptresist: 0
+  use Ecto.Schema
+
+  schema "backpacks" do
+    field(:name, :string)
+    field(:set, :string, default: "")
+    field(:class, :string)
+    field(:capacity, :integer, default: 0)
+    field(:protection, :integer, default: 0)
+    field(:invent_prot, :integer, default: 0)
+    field(:status_resist, :integer, default: 0)
+    field(:movespeed, :integer, default: 0)
+    field(:stamina_cost_reduction, :integer, default: 0)
+    field(:effects, :string, default: "")
+    field(:preservation, :integer, default: 0)
+    field(:physical_bonus, :integer, default: 0)
+    field(:lightning_bonus, :integer, default: 0)
+    field(:corruptresist, :integer, default: 0)
+  end
 end
