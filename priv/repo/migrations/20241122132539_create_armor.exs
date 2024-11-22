e29d8469fdb2968cfb8d4b7a51db365383f28282
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-22 15:44:22 +0200

    more migrations

--- priv/repo/migrations/20241122132539_create_armor.exs
+++ priv/repo/migrations/20241122132539_create_armor.exs
@@ -1,34 +1,39 @@
-defmodule OutwardPlanner.Repo.Migrations.CreateWeapons do
+defmodule OutwardPlanner.Repo.Migrations.CreateArmor do
   use Ecto.Migration
 
   def change do
-    create table :weapons do
+    create table :armor do
       add(:name, :string)
       add(:class, :string)
       add(:type, :string)
       add(:set, :string)
       add(:protection, :integer)
       add(:barrier, :integer)
-      add(:impact_resistance, :integer)
       add(:mana_cost_reduction, :integer)
-      add(:stamcost, :integer)
-      add(:attackspeed, :integer)
+      add(:stamina_cost_reduction, :integer)
+      add(:movespeed, :integer)
+      add(:cooldown, :integer)
       add(:effects, :string)
-      add(:impact, :integer)
-      add(:physical, :integer)
-      add(:physical_bonus, :integer)
+      add(:pouch, :integer)
       add(:decay, :integer)
       add(:decay_bonus, :integer)
-      add(:ethereal_bonus, :integer)
       add(:ethereal, :integer)
+      add(:ethereal_bonus, :integer)
       add(:fire, :integer)
       add(:fire_bonus, :integer)
       add(:frost, :integer)
       add(:frost_bonus, :integer)
+      add(:impact, :integer)
       add(:lightning, :integer)
       add(:lightning_bonus, :integer)
+      add(:physical, :integer)
+      add(:physical_bonus, :integer)
+      add(:coldresist, :integer)
+      add(:heatresist, :integer)
+      add(:corruptresist, :integer)
+      add(:status_resist, :integer)
     end
 
-  create unique_index(:weapons, [:name])
+  create unique_index(:armor, [:name])
   end
 end
---
+++ priv/repo/migrations/20241122132544_create_skills.exs
@@ -0,0 +1,15 @@
+defmodule OutwardPlanner.Repo.Migrations.CreateSkills do
+  use Ecto.Migration
+
+  def change do
+    create table :skills do
+      add(:name, :string)
+      add(:effects, :string)
+      add(:skilltype, :string)
+      add(:skilltier, :integer)
+      add(:subtype, :string)
+    end
+
+  create unique_index(:skills, [:name])
+  end
+end
