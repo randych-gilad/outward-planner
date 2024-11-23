ad4479b73b86723d4fb6caf8dc70a09075fbe81f
Nathan Yam <nathancyam@gmail.com>
2024-11-23 12:18:11 +1100

    Add phoenix

---
+++ lib/outward_planner_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule OutwardPlannerWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use OutwardPlannerWeb, :controller` and
+  `use OutwardPlannerWeb, :live_view`.
+  """
+  use OutwardPlannerWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/outward_planner_web/router.ex
@@ -0,0 +1,44 @@
+defmodule OutwardPlannerWeb.Router do
+  use OutwardPlannerWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {OutwardPlannerWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", OutwardPlannerWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", OutwardPlannerWeb do
+  #   pipe_through :api
+  # end
+
+  # Enable LiveDashboard and Swoosh mailbox preview in development
+  if Application.compile_env(:outward_planner, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
+
+    scope "/dev" do
+      pipe_through :browser
+
+      live_dashboard "/dashboard", metrics: OutwardPlannerWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
