13a784845fa34b3ec9244b37c932bb21abd0bc13
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-23 18:53:03 +0200

    basic route + page

--- lib/outward_planner_web/router.ex
+++ lib/outward_planner_web/router.ex
@@ -17,7 +17,8 @@ defmodule OutwardPlannerWeb.Router do
   scope "/", OutwardPlannerWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    # get "/", PageController, :home
+    live "/", Page
   end
 
   # Other scopes may use custom stacks.
