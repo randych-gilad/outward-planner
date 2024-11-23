4eae22f6f9db8fbf102b25bb98a1146e2738ce75
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-23 20:03:46 +0200

    basic dropdown

--- lib/outward_planner_web/live/page.ex
+++ lib/outward_planner_web/live/page.ex
@@ -1,20 +1,38 @@
 defmodule OutwardPlannerWeb.Page do
   use OutwardPlannerWeb, :live_view
+  import Ecto.Query, except: [update: 3]
+  alias OutwardPlanner.Repo
 
   def render(assigns) do
     ~H"""
-    Current temperature: <%= @temperature %>°F
-    <button phx-click="inc_temperature">+</button>
+    <label for="weapons-dropdown">Choose a weapon:</label>
+    <select id="weapons-dropdown" name="weapons-dropdown">
+      <%= for weapon <- @weapons do %>
+        <option value="{@weapon}"><%= weapon %></option>
+      <% end %>
+    </select>
+
+    <label for="armors-dropdown">Choose an armor:</label>
+    <select id="armors-dropdown" name="armors-dropdown">
+      <%= for armor <- @armors do %>
+        <option value="{@armor}"><%= armor %></option>
+      <% end %>
+    </select>
+
+    <label for="skills-dropdown">Choose a spell:</label>
+    <select id="skills-dropdown" name="spells-dropdown">
+      <%= for skill <- @skills do %>
+        <option value="{@skill}"><%= skill %></option>
+      <% end %>
+    </select>
     """
   end
 
   def mount(_params, _session, socket) do
-    # Let's assume a fixed temperature for now
-    temperature = 70
-    {:ok, assign(socket, :temperature, temperature)}
-  end
+    weapons = Repo.all(from w in OutwardPlanner.Stats.Weapon, select: w.name)
+    armors = Repo.all(from a in OutwardPlanner.Stats.Armor, select: a.name)
+    skills = Repo.all(from s in OutwardPlanner.Stats.Skill, select: s.name)
 
-  def handle_event("inc_temperature", _params, socket) do
-    {:noreply, update(socket, :temperature, &(&1 + 1))}
+    {:ok, assign(socket, weapons: weapons, armors: armors, skills: skills)}
   end
 end
