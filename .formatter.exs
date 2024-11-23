ad7f0d6ad8edf2a6a21a6ac982e97742ddf4d5e4
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-23 18:52:51 +0200

    formatting

--- .formatter.exs
+++ .formatter.exs
@@ -4,5 +4,26 @@
     "{config,lib,test}/**/*.{ex,exs}",
     "priv/repo/migrations/*.exs"
   ],
+  locals_without_parens: [
+    add: 2,
+    add: 3,
+    remove: 2,
+    remove: 3,
+    modify: 2,
+    modify: 3,
+    create: 1,
+    create: 2,
+    create_if_not_exists: 1,
+    create_if_not_exists: 2,
+    drop: 1,
+    drop: 2,
+    get: 3,
+    plug: 1,
+    plug: 2,
+    live: 2,
+    pipe_through: 1,
+    live_dashboard: 2,
+    forward: 2
+  ],
   import_deps: [:ecto, :ecto_sql]
 ]
