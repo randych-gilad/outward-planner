8e0972174318dfd7e66c7988f20a3c0e2f59e9cc
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-11-23 02:22:30 +0200

    formatting

--- priv/repo/migrations/20241122132534_create_backpacks.exs
+++ priv/repo/migrations/20241122132534_create_backpacks.exs
@@ -2,23 +2,23 @@ defmodule OutwardPlanner.Repo.Migrations.CreateBackpacks do
   use Ecto.Migration
 
   def change do
-    create table :backpacks do
-      add(:name, :string)
-      add(:set, :string)
-      add(:class, :string)
-      add(:capacity, :integer)
-      add(:protection, :integer)
-      add(:invent_prot, :integer)
-      add(:status_resist, :integer)
-      add(:movespeed, :integer)
-      add(:stamina_cost_reduction, :integer)
-      add(:effects, :string)
-      add(:preservation, :integer)
-      add(:physical_bonus, :integer)
-      add(:lightning_bonus, :integer)
-      add(:corruptresist, :integer)
+    create table(:backpacks) do
+      add :name, :string
+      add :set, :string
+      add :class, :string
+      add :capacity, :integer
+      add :protection, :integer
+      add :invent_prot, :integer
+      add :status_resist, :integer
+      add :movespeed, :integer
+      add :stamina_cost_reduction, :integer
+      add :effects, :string
+      add :preservation, :integer
+      add :physical_bonus, :integer
+      add :lightning_bonus, :integer
+      add :corruptresist, :integer
     end
 
-  create unique_index(:backpacks, [:name])
+    create unique_index(:backpacks, [:name])
   end
 end
--- priv/repo/migrations/20241122132544_create_skills.exs
+++ priv/repo/migrations/20241122132544_create_skills.exs
@@ -2,14 +2,14 @@ defmodule OutwardPlanner.Repo.Migrations.CreateSkills do
   use Ecto.Migration
 
   def change do
-    create table :skills do
-      add(:name, :string)
-      add(:effects, :string)
-      add(:skilltype, :string)
-      add(:skilltier, :integer)
-      add(:subtype, :string)
+    create table(:skills) do
+      add :name, :string
+      add :effects, :string
+      add :skilltype, :string
+      add :skilltier, :integer
+      add :subtype, :string
     end
 
-  create unique_index(:skills, [:name])
+    create unique_index(:skills, [:name])
   end
 end
